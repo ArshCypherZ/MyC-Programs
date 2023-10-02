abda346348afd23bf4330bd8f5739d4a4c20f306
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2023-10-02 10:00:01 +0530

    Reducing variables scope

--- 4.c
+++ 4.c
@@ -4,12 +4,12 @@ void main()
 
 
 // Reverse the number
-int n, i, rem, rev=0;
+int n, rev=0;
 printf("Enter number to reverse: ");
 scanf("%d", &n);
 while (n!=0)
 {
-	rem = n % 10;
+	int rem = n % 10;
 	rev = rev * 10 + rem;
 	n = n / 10;
 }
@@ -17,13 +17,13 @@ printf("Reversed number is: %d", rev);
 
 
 // Fibonacci Series
-int a, c, num, x=0, y=1;
+int a, c, x=0, y=1;
 printf("\n\nEnter number of terms for fibonacci series: ");
 scanf("%d", &a);
 printf("0, ");
 for (c=1; c<a; c++)
 {
-	num = x + y;
+	int num = x + y;
 	printf("%d, ", num);
 	y = x;
 	x = num;
@@ -46,7 +46,7 @@ printf("Average: %f", avg);
 
 
 // Palindrome Number
-int number, remainder, reverse=0, temp;
+int number, reverse=0, temp;
 printf("\n\nEnter number to check for palindrome: ");
 scanf("%d", &number);
 
@@ -54,7 +54,7 @@ temp = number;
 
 while (number!=0)
 {
-	remainder = number % 10;
+	int remainder = number % 10;
 	reverse = reverse * 10 + remainder;
 	number = number/10;
 }
